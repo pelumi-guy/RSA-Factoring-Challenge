8f9abd4413638911b3dd6dbe94e8dd2058327325
pelumi <polalekan@gmail.com>
2022-10-14 16:43:31 +0100

    updated solution

--- factors.py
+++ factors.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from math import floor
 """A Program to factorize as many numbers as
 possible into a product of two smaller numbers."""
 
-file_name = sys.argv[1]
+if len(argv) != 2:
+    exit()
 
-with open(file_name) as fp:
-    while True:
-        line = fp.readline()
-        if not line:
-            break
+file_name = argv[1]
 
-        num = int(line)
-        for fac in range (2, (floor(num ** 0.5) + 1)):
+def factorize(num):
+    """factorize a given number into 2 factors"""
+    if num < 2:
+        return
+    for fac in range (2, (floor(num ** 0.5) + 1)):
             if num % fac == 0:
+                print("{:.0f}={:.0f}*{:.0f}".format(num, num // fac, fac))
+                return
+
+try:
+    with open(file_name) as fp:
+        while True:
+            line = fp.readline()
+            if not line:
                 break
-        if fac == floor(num ** 0.5) and num % fac != 0:
-            print("{}={}*{}".format(num, num, 1))
-        else:
-            fac2 = num // fac
-            print("{}={}*{}".format(num, fac2, fac))
+
+            num = int(line.split('\n')[0])
+            factorize(num)
+except:
+    pass
+
